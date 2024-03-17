bb1a5b42511e37c0a1b254c8a41265b2e1c8fb39
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-18 00:21:57 +0200

    SVG micro animations - animated Home SVG

--- 03-SVG-Micro Animations/app.js
+++ 03-SVG-Micro Animations/app.js
@@ -11,3 +11,35 @@ const home = document.querySelector('.home')
 const notifications = document.querySelector('.notifications')
 const messages = document.querySelector('.messages')
 
+// Animating the Home SVG element
+// 1. setting some initial styles of feather
+gsap.set('.feather', { scale: 0, transformOrigin: 'center' })
+// 2. triggering the animation on the home click
+home.addEventListener('click', () => {
+  // whole home animation
+  gsap.fromTo(
+    '.home-svg',
+    { scale: 1 },
+    {
+      scale: 0.9,
+      yoyo: true,
+      repeat: 1,
+    }
+  )
+  // feather animation
+  gsap.fromTo(
+    '.feather',
+    {
+      y: -5,
+      scale: 0,
+    },
+    {
+      y: 20,
+      scale: 1.5,
+      duration: 1,
+      stagger: 0.2,
+    }
+  )
+  // right feather nimation
+  gsap.fromTo('.right-feather', { x: 0 }, { x: 5 })
+})
