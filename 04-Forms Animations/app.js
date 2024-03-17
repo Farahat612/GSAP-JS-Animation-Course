6a45a49036e42e44071e43a54c7a80c47260eff1
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-18 01:47:37 +0200

    Forms Animations - Selected elements in the script and created default timeline

---
+++ 04-Forms Animations/app.js
@@ -0,0 +1,6 @@
+// Selecting the elements needed from the document
+const containers = document.querySelectorAll('.input-container')
+const form = document.querySelector('form')
+
+// Creating a GSAP default timeline
+const tl = gsap.timeline({ defaults: { duration: 1 } })
