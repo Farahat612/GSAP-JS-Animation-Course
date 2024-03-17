7dfecade6049814a81b5a9b8dcc1c1ee5419fd34
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-18 01:50:21 +0200

    Forms Animations - created line start and end positions

--- 04-Forms Animations/app.js
+++ 04-Forms Animations/app.js
@@ -4,3 +4,9 @@ const form = document.querySelector('form')
 
 // Creating a GSAP default timeline
 const tl = gsap.timeline({ defaults: { duration: 1 } })
+
+// creating start and end positions for the line svg to achieve the elastic effect
+const start =
+  'M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512'
+const end =
+  'M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512'
