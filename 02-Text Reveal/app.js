7e87e9be4923c3fec30ee12c653d9a003fe1d178
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-17 22:15:42 +0200

    Text Reveal animation - animated the first line of cta text

--- 02-Text Reveal/app.js
+++ 02-Text Reveal/app.js
@@ -7,9 +7,8 @@ const tl = gsap.timeline({
 tl.fromTo(
   '.hero-img',
   {
-    scale: 1.5,
+    scale: 1.3,
     borderRadius: '0rem',
-    duration: 1.5,
   },
   {
     scale: 1,
@@ -19,3 +18,42 @@ tl.fromTo(
     ease: 'elastic.out(1.5, 1)',
   }
 )
+
+// Animating the first line of cta text to reveal from the left to the right
+tl.fromTo(
+  '.cta-1',
+  {
+    x: '100%',
+    opacity: 0.5,
+  },
+  {
+    x: 0,
+    opacity: 1,
+  },
+  '<20%'
+) // <20% means it should start 20% before the hero animation ends
+tl.fromTo(
+  '.cta-3',
+  {
+    x: '-100%',
+    opacity: 0.5,
+  },
+  {
+    x: 0,
+    opacity: 1,
+  },
+  '<20%'
+) // <20% means it should start 20% before the cta-1 animation ends
+tl.fromTo(
+  '.cta-2',
+  {
+    y: '100%',
+    opacity: 0.5,
+  },
+  {
+    y: 0,
+    opacity: 1,
+  },
+  '<20%'
+) // <20% means it should start 20% before the cta-3 animation ends
+
