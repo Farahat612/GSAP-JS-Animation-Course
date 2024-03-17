e89b9a64f98257ff605195dc45ff5145193859fb
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-18 00:16:00 +0200

    SVG micro animations - selecting elements from the document

--- 03-SVG-Micro Animations/app.js
+++ 03-SVG-Micro Animations/app.js
@@ -1,7 +1,13 @@
-// Creating a timeline with GSAP for the SVG animation
+// Creating a default timeline with GSAP for the SVG animation
 const tl = gsap.timeline({
   defaults: {
     duration: 0.35,
-    ease: 'Power2.easeOut',
+    ease: 'Power2.out',
   },
 })
+
+// selecting the three SVG elements from the document
+const home = document.querySelector('.home')
+const notifications = document.querySelector('.notifications')
+const messages = document.querySelector('.messages')
+
