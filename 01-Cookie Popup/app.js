3dec636b5766d24748a499ae3daef1ee9f21655a
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-17 21:05:09 +0200

    cookie js animation - animated container, cookie and text

--- 01-Cookie Popup/app.js
+++ 01-Cookie Popup/app.js
@@ -1,2 +1,18 @@
-// create a timeline for the animations
+// create a timeline for the animations ---> tl stands for timeline
 const tl = gsap.timeline({ defaults: { duration: 0.75, ease: 'power1.out' } })
+
+// animate the cookie container
+tl.fromTo(
+  '.cookie-container',
+  { scale: 0 },
+  { scale: 1, ease: 'elastic.out(1, 0.4)', duration: 1.5 }
+)
+// animate the cookie itself
+tl.fromTo(
+  '.cookie',
+  { opacity: 0, x: -50, rotation: '-45deg' },
+  { opacity: 1, x: 0, rotation: '0deg' },
+  '<50%'
+) // '<50%' means it will start at 50% of the previous animation
+// animate the text
+tl.fromTo('.text', { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, '<') // '<' means it will start at the same time as the previous animation
