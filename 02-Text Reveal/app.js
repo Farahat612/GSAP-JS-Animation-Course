25ced42d476e774c63e1d450e4d4683b995471d5
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-17 22:18:07 +0200

    Text Reveal animation - animated the second line of cta text

--- 02-Text Reveal/app.js
+++ 02-Text Reveal/app.js
@@ -1,6 +1,6 @@
 // creating a GSAP default timeline for the text reveal animation
 const tl = gsap.timeline({
-  defaults: { duration: 0.75, ease: 'Power3.out' },
+  defaults: { duration: 0.45, ease: 'Power3.out' },
 })
 
 // Animating the hero image to start from taking up the full width of the viewport and then scaling down to its original size
@@ -46,6 +46,45 @@ tl.fromTo(
 ) // <20% means it should start 20% before the cta-1 animation ends
 tl.fromTo(
   '.cta-2',
+  {
+    y: '100%',
+    opacity: 0.5,
+  },
+  {
+    y: 0,
+    opacity: 1,
+  },
+  '<20%'
+) // <20% means it should start 20% before the cta-3 animation ends
+
+
+// Animating the second line of cta text to reveal from the left to the right
+tl.fromTo(
+  '.cta-4',
+  {
+    x: '100%',
+    opacity: 0.5,
+  },
+  {
+    x: 0,
+    opacity: 1,
+  },
+  '<20%'
+) // <20% means it should start 20% before the hero animation ends
+tl.fromTo(
+  '.cta-6',
+  {
+    x: '-100%',
+    opacity: 0.5,
+  },
+  {
+    x: 0,
+    opacity: 1,
+  },
+  '<20%'
+) // <20% means it should start 20% before the cta-1 animation ends
+tl.fromTo(
+  '.cta-5',
   {
     y: '100%',
     opacity: 0.5,
