2ed7708670c1b8d55f11088d50b066e1db8d6118
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-17 22:05:45 +0200

    Text Reveal animation - animated hero image

--- 02-Text Reveal/app.js
+++ 02-Text Reveal/app.js
@@ -1,4 +1,21 @@
 // creating a GSAP default timeline for the text reveal animation
 const tl = gsap.timeline({
-  defaults: { duration: 0.75, ease: 'Power3.easeOut' },
+  defaults: { duration: 0.75, ease: 'Power3.out' },
 })
+
+// Animating the hero image to start from taking up the full width of the viewport and then scaling down to its original size
+tl.fromTo(
+  '.hero-img',
+  {
+    scale: 1.5,
+    borderRadius: '0rem',
+    duration: 1.5,
+  },
+  {
+    scale: 1,
+    borderRadius: '2rem',
+    delay: 0.35,
+    duration: 2.5,
+    ease: 'elastic.out(1.5, 1)',
+  }
+)
