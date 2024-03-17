c303c2286672675a958c4f719005e4d803875ebe
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-17 21:11:13 +0200

    cookie js animation - animated fading the popup out when the button is clicked

--- 01-Cookie Popup/app.js
+++ 01-Cookie Popup/app.js
@@ -1,11 +1,11 @@
-// create a timeline for the animations ---> tl stands for timeline
+// create a default timeline for the animations ---> tl stands for timeline
 const tl = gsap.timeline({ defaults: { duration: 0.75, ease: 'power1.out' } })
 
 // animate the cookie container
 tl.fromTo(
   '.cookie-container',
   { scale: 0 },
-  { scale: 1, ease: 'elastic.out(1, 0.4)', duration: 1.5 }
+  { scale: 1, ease: 'elastic.out(1, 0.4)', duration: 1.5 } // the elastic.out is a custom ease from the GSAP library
 )
 // animate the cookie itself
 tl.fromTo(
@@ -16,3 +16,10 @@ tl.fromTo(
 ) // '<50%' means it will start at 50% of the previous animation
 // animate the text
 tl.fromTo('.text', { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, '<') // '<' means it will start at the same time as the previous animation
+
+
+//Fading the cookie out when the button is clicked
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    gsap.to('.cookie-container', {opacity: 0, y: 100, duration: 0.45, ease: 'power1.out'})
+})
