9714714d7cc97ba8a5bdd0c85a8e26c555152d47
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-19 00:51:50 +0200

    IPhone Website : Animattion : animated the text hilighting and fading out in the second page

--- 06-IPhone Website/app.js
+++ 06-IPhone Website/app.js
@@ -9,4 +9,30 @@ const tlIntro = gsap.timeline({
   },
 })
 
-
+// 02. Animating the text highlighting in the second section during the scroll to highlight the text while it's getting in the viewport and then fade it out when it's going out of the viewport
+// 2.1 Animating the highlighting part
+const tlH = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.second-page', // the element that will trigger the animation
+    // markers: { startColor: 'blue', endColor: 'blue' },
+    scrub: true, // this will make the animation smooth and not jumpy
+    start: '-40%', // when to start the animation --> minus means before the element reaches it's place the viewport to start sooner
+    end: '40%', // when to end the animation --> plus means after the element reaches it's place the viewport to end later
+  },
+})
+tlH.fromTo(
+  '.highlight',
+  { color: 'rgba(255,255,255, 0.4' },
+  { color: 'rgba(255,255,255, 1', stagger: 1 }
+)
+// 2.2 Animating the fade out part
+const tlHRemove = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.second-page',
+    // markers: { startColor: 'pink', endColor: 'pink' },
+    scrub: true,
+    start: '-20%',
+    end: '60%',
+  },
+})
+tlHRemove.to('.highlight', { color: 'rgba(255,255,255, 0.4', stagger: 1 })
