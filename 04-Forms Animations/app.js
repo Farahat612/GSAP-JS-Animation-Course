a5696914fcd616e671a501e1647be0a7771a643a
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-18 02:10:08 +0200

    Forms Animations - added the elastic bounce effect to the svg line in all inputs on focus

--- 04-Forms Animations/app.js
+++ 04-Forms Animations/app.js
@@ -6,7 +6,44 @@ const form = document.querySelector('form')
 const tl = gsap.timeline({ defaults: { duration: 1 } })
 
 // creating start and end positions for the line svg to achieve the elastic effect
+// *Note: the values are hardcoded and got from figma design made by the course author*
 const start =
   'M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512'
 const end =
   'M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512'
+
+// Looping through the containers and adding the elastic effect to each one being focused
+
+containers.forEach((container) => {
+  // 1. Selecting the elements needed from the container
+  const input = container.querySelector('.input')
+  const line = container.querySelector('.elastic-line')
+  const placeholder = container.querySelector('.placeholder')
+
+  input.addEventListener('focus', () => {
+    //Checking to see if there is any text in the input
+    if (!input.value) {
+      tl.fromTo(
+        // Animating this specific line from the start to the end
+        line,
+        // manipulating the line's "d" attribute in the path --> check the SVG documentation
+        { attr: { d: start } },
+        {
+          attr: { d: end },
+          ease: 'Power2.easeOut',
+          duration: 0.75,
+        }
+      )
+      tl.to(
+        // Animating the line back to the start position to achieve the elastic bounce effect
+        line,
+        {
+          attr: { d: start },
+          ease: 'elastic.out(3,0.5)', // using GSAP cutom ease to achieve the bounce effect
+        },
+        '<50%'
+      )
+      
+    }
+  })
+})
